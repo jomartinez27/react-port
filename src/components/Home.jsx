a8e193bb8ba0cc96181882accc866259683c33e3
Jose Martinez <josmmartinez94@gmail.com>
2018-11-15 15:13:25 -0800

    Add boiler plate

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import './Home.css'
+
+class Home extends React.Component {
+  render () {
+    <Grid>
+      <Jumbotron>
+        <h2>Welcome to my portfolio</h2>
+        <p>I am a software developer, specializing in Front End Development</p>
+      </Jumbotron>
+      <Link to="/about">
+        <Button bsStyle="primary">About</Button>
+      </Link>
+    </Grid>
+  }
+}
+
+export default Home;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Projects extends React.Component {
+  render () {
+    return (
+      <div>
+        Projects
+      </div>
+    )
+  }
+}
+
+export default Projects;

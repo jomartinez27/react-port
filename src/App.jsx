99913c89d4ccce3d3c43278c4a5cb0d4ef18831a
Jose Martinez <josmmartinez94@gmail.com>
2018-11-15 15:31:45 -0800

    Add custom nav bar to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
+import Navbar from './components/CustomNav';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <Navbar />
           <Route exact path="/" component={Home}/>
           <Route exact path="/about" component={About}/>
           <Route exact path="/projects" component={Projects}/>
--- src/components/CustomNav.jsx
+++ src/components/CustomNav.jsx
@@ -8,9 +8,23 @@ class CustomNav extends React.Component {
       <Navbar default collapseOnSelct>
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to="/">Home</Link>
+            <Link to="/">Jose</Link>
           </Navbar.Brand>
+          <Navbar.Toggle />
         </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} componentClass={Link} href="/" to="/">
+              Home
+            </NavItem>
+            <NavItem eventKey={2} componentClass={Link} href="/" to="/about">
+              About
+            </NavItem>
+            <NavItem eventKey={3} componentClass={Link} href="/" to="/projects">
+              Projects
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     )
   }

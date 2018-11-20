356cfed0d0d21ddeb35ce26208d571bbdfb39ed1
Jose Martinez <josmmartinez94@gmail.com>
2018-11-19 17:10:11 -0800

    Add color for h3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ class About extends React.Component {
         <Grid>
           <Col xs={12} sm={8} smOffset={2}>
             <Image src="assets/Jose Khal Rest Cropped.jpg" className="about-profile-pic"/>
-            <h3>Jose Martinez</h3>
+            <h3>Jose & Khal resting</h3>
             <p>This is my dog, Khal Drogo, named after Jason Mamoa's character on GOT. Being a doggy dad can be challenging, but resting is nice.</p>
           </Col>
         </Grid>

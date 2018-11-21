aab91f5245086a0b9506c9785c3dc4222b2a1321
Jose Martinez <josmmartinez94@gmail.com>
2018-11-20 19:50:44 -0800

    Change about text, change layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,37 +9,36 @@ class About extends React.Component {
         <Image src="assets/About Banner Cropped.jpg" className="header-img" />
         <Grid className="about-container">
           <Col xs={12} sm={8} smOffset={2}>
-            <h3>Hello, my name is, Jose Martinez</h3>
-            <p>
-              My interest in software development was first sparked in high
-              school while taking a multimedia class. Being able to flex my
-              creative muscle, problem solving and thinking things logically
-              are the reasons why I enjoy software development. I have a
-              passion for web and mobile apps and the fast paced world of
-              technology. A strong team member, a desire to learn and pick
-              brains of those around me. In the past I have built a full-stack
-              app called PixelPx. A site where users can upload and share images
-              with other users with the community being focused on pixelated art.
-              Currently I am working on a JavaScript game, a point and click game
-              where users click on a falling particle in order to score points :)
-
-              Languages: Ruby, JS, Rails, React, CSS, HTML.
-              Outside of programming: Powerlifting, video games, archery, and being a dog parent.
-            </p>
-            <Carousel >
-              <Carousel.Item>
-                <Image src="assets/Jose Amy a_A Cropped.jpg" className=".about-profile-pic"/>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Image src="assets/Jose Khal Rest Cropped.jpg" className=".about-profile-pic"/>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Image src="assets/Jose Snow Cropped.jpg" className=".about-profile-pic"/>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Image src="assets/Jose Khal Beach Cropped.jpg" className=".about-profile-pic"/>
-              </Carousel.Item>
-            </Carousel>
+                <Carousel >
+                  <Carousel.Item>
+                    <Image src="assets/Jose Amy a_A Cropped.jpg" className=".about-profile-pic"/>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <Image src="assets/Jose Khal Rest Cropped.jpg" className=".about-profile-pic"/>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <Image src="assets/Jose Snow Cropped.jpg" className=".about-profile-pic"/>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <Image src="assets/Jose Khal Beach Cropped.jpg" className=".about-profile-pic"/>
+                  </Carousel.Item>
+                </Carousel>
+                  <h3>Hello, my name is, Jose Martinez</h3>
+                  <p>
+                    My interest in software development was first sparked in high
+                    school while taking a multimedia class. Being able to flex my
+                    creative muscle, problem solving and thinking things logically
+                    are the reasons why I enjoy software development. I have a
+                    passion for web and mobile apps and the fast paced world of
+                    technology. A strong team member, a desire to learn and pick
+                    brains of those around me. In the past I have built a full-stack React/Rails app,
+                    a JavaScript game, a MERN stack app, and a React SPA as well as my own portfolio page.
+                    <br />
+                    <br />
+                    Languages: Ruby/Rails, JavaScript/Node.js, React.js/Redux, CSS, HTML5.
+                    <br />
+                    Outside of programming: Powerlifting, video games, archery, and being a dog parent.
+                  </p>
           </Col>
         </Grid>
       </div>

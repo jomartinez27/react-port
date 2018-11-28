ca7dd1071f8e932e70b922b3c144cc21bf6969cf
Jose Martinez <josmmartinez94@gmail.com>
2018-11-27 16:51:52 -0800

    Start projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Grid, Col, Row, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import "./Projects.css"
 
 class Projects extends React.Component {
   render () {
     return (
       <div>
-        <Grid>
+        <Grid fluid={true}>
           <Row>
             <Col>
-              <Image src="assets/ult-home.png" width={400}/>
+              <Image src="assets/ult-home Cropped.png" width={400}/>
+            </Col>
+            <Col>
+              <h3>Ultimate Movie Guide</h3>
+              <p>
+                The Ultimate Movie Guide is a Single Page App,
+                that is built using React/Redux. This web app utilizes
+                MovieDB's API to fetch data then displays the movies based
+                on what the user sorts by: "Now Playing", "Top Rated", or
+                "Most Popular". The user can also click on a movie
+                to get more details about a specific movie.
+              </p>
+            </Col>
+            <Col>
+              <Image src="assets/ult-movie-details Cropped.png" width={400}/>
             </Col>
           </Row>
         </Grid>

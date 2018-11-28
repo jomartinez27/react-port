c86353f478107f383ff420aceeb84a3abd45cead
Jose Martinez <josmmartinez94@gmail.com>
2018-11-27 18:28:53 -0800

    Change layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,30 +4,43 @@ import { Link } from 'react-router-dom';
 import "./Projects.css"
 
 class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleImage = this.handleImage.bind(this);
+    this.handleLeave = this.handleLeave.bind(this);
+  }
+
+  handleImage(e) {
+    return e.target.src = "assets/ult-movie-details Cropped.png"
+  }
+
+  handleLeave(e) {
+    return e.target.src = "assets/ult-home Cropped.png"
+  }
+
   render () {
     return (
       <div>
         <Grid>
-          <Row className="proj">
-            <Col xs={12} sm={1}>
-              <Image src="assets/ult-home Cropped.png" width={400}/>
+          <Row className="proj-1">
+            <Col xs={12} sm={4}>
+              <Image src="assets/ult-home Cropped.png"
+                rounded
+                responsive
+                onMouseEnter={this.handleImage}
+                onMouseLeave={this.handleLeave}
+                />
             </Col>
             <Col xs={12} sm={8}>
-              <h3 style={{color: "#FFF"}}>Ultimate Movie Guide</h3>
+              <h3>Ultimate Movie Guide</h3>
               <p>
-                The Ultimate Movie Guide is a Single Page App,
-                that is built using React/Redux. This web app utilizes
-                MovieDB's API to fetch data then displays the movies based
-                on what the user sorts by: "Now Playing", "Top Rated", or
-                "Most Popular". The user can also click on a movie
-                to get more details about a specific movie.
+                The Ultimate Movie Guide is a Single Page App, built using
+                React/Redux. The web app utilizes MovieDB's API to fetch
+                data, then displays movies according to what the user sorts
+                by: "Top Rated", "Now Playing", "Most Popular". The user can
+                also click on a movie title to get more information on a specific
+                movie
               </p>
-            </Col>
-            <Col xs={12} sm={1}>
-              <Image src="assets/ult-sorted Cropped.png" width={400}/>
-            </Col>
-            <Col sm={12}>
-              <Image src="assets/ult-movie-details Cropped.png" className="proj-image"/>
             </Col>
           </Row>
         </Grid>

d7f11935785123ad4d54f4381b293ae5df4e3b12
Jose Martinez <josmmartinez94@gmail.com>
2018-11-28 12:41:30 -0800

    Add canvas to home.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,8 +9,8 @@ class About extends React.Component {
       <div>
         <Grid className="about-container" fluid>
           <Jumbotron>
-            <h3>About Me</h3>
-            <p>I am a software developer, specializing in Front End Development</p>
+            <h3 className="jumb-text">Hello, my name is, Jose Martinez</h3>
+            <p className="jumb-text">I am a software developer, specializing in Front End Development</p>
             <a target="_blank" href="https://www.linkedin.com/in/jose-martinez-517a29149/"><Button bsStyle="primary">LinkedIn</Button></a>
             <a target="_blank" href="https://github.com/jomartinez27"><Button bsStyle="primary">GitHub</Button></a>
           </Jumbotron>
@@ -32,7 +32,7 @@ class About extends React.Component {
               </Carousel>
             </Col>
             <Col className="about-desc">
-              <h3 className="about-h3">Hello, my name is, Jose Martinez</h3>
+              <h3 className="about-h3">About me</h3>
               <p>
                 My interest in software development was first sparked in high
                 school while taking a multimedia class. Being able to flex my
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,20 @@ import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
 import './Home.css'
 
 class Home extends React.Component {
+  componentDidMount() {
+    const canvas = this.refs.canvas
+    const ctx = canvas.getContext('2d');
+    
+  }
+
   render () {
     return (
       <Grid>
+        <Row>
+          <Col>
+            <canvas ref="canvas" width={640} height={425}></canvas>
+          </Col>
+        </Row>
       </Grid>
     )
   }

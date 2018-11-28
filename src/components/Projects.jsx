545945a2c21545960ef10e2e09369c639fc8ed71
Jose Martinez <josmmartinez94@gmail.com>
2018-11-28 11:59:03 -0800

    Move projects to projects.jsx, change home.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,46 +1,36 @@
 import React from 'react';
-import { Grid, Col, Row, Image } from 'react-bootstrap';
+import { Grid, Col, Row, Image, Jumbotron } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import "./Projects.css"
 
 class Projects extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleImage = this.handleImage.bind(this);
-    this.handleLeave = this.handleLeave.bind(this);
-  }
-
-  handleImage(e) {
-    return e.target.src = "assets/ult-movie-details Cropped.png"
-  }
-
-  handleLeave(e) {
-    return e.target.src = "assets/ult-home Cropped.png"
-  }
-
   render () {
     return (
       <div>
         <Grid>
-          <Row className="proj-1">
-            <Col xs={12} sm={4}>
-              <Image src="assets/ult-home Cropped.png"
-                rounded
-                responsive
-                onMouseEnter={this.handleImage}
-                onMouseLeave={this.handleLeave}
-                />
-            </Col>
-            <Col xs={12} sm={8}>
+          <Jumbotron id="jumbotron">
+            <Image src="assets/About Banner Cropped.jpg" responsive rounded/>
+          </Jumbotron>
+          <Row className="show-grid text-center">
+            <Col xs={12} sm={4} className="projects-wrapper">
+              <Image src="assets/ult-home Cropped.png" circle className="projects-pic" />
               <h3>Ultimate Movie Guide</h3>
-              <p>
-                The Ultimate Movie Guide is a Single Page App, built using
-                React/Redux. The web app utilizes MovieDB's API to fetch
-                data, then displays movies according to what the user sorts
-                by: "Top Rated", "Now Playing", "Most Popular". The user can
-                also click on a movie title to get more information on a specific
-                movie
-              </p>
+              <p>Ultimate Movie Guide is a web app that uses MovieDB API to display movies by top rated, popular, or now playing</p>
+            </Col>
+            <Col xs={12} sm={4} className="projects-wrapper">
+              <Image src="assets/logoCover.png" circle className="projects-pic" />
+              <h3>Let's Eat</h3>
+              <p>Let's Eat is a web app that uses Yelp's API to randomly generate a restaurant based on location and Google Maps API to display where the restaurant is located</p>
+            </Col>
+            <Col xs={12} sm={4} className="projects-wrapper">
+              <Image src="assets/FireWorks Cropped.png" circle className="projects-pic" />
+              <h3>FireWorks</h3>
+              <p>Fireworks is a JavaScript game that simulates a firework explosion. The user then clicks on the rainbow colored particle to score points and keep the game going</p>
+            </Col>
+            <Col xs={12} sm={4} className="projects-wrapper">
+              <Image src="assets/pixel-px-home Cropped.png" circle className="projects-pic" />
+              <h3>PixelPx</h3>
+              <p>PixelPx is a web app that is inspired by 500px where users upload and share images with other users with the focus on pixelated art</p>
             </Col>
           </Row>
         </Grid>
